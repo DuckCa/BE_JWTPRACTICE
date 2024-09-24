192b51548d39e483daef5b2eac5a244015590404
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-09-24 20:23:54 +0700

    step 5

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, handleLogin } = require("../controllers/userController");
 const routerAPI = express.Router();
 
 routerAPI.get("/", (req, res) => {
   return res.status(200).json("HELLO WORLD API");
 });
 routerAPI.post("/register", createUser);
+routerAPI.post("/login", handleLogin);
 module.exports = routerAPI; //export default
--- src/services/userService.js
+++ src/services/userService.js
@@ -16,7 +16,33 @@ const createUserService = async (name, email, password) => {
     return null;
   }
 };
+const loginService = async (email, password) => {
+  try {
+    const user = await User.findOne({ email: email }).exec();
+    if (user) {
+      const isMatchPassword = await bycrypt.compare(password, user.password);
+      console.log(">>>>>>>>CHECK ismatchPass:", isMatchPassword);
 
+      if (!isMatchPassword) {
+        return {
+          EC: 2,
+          EM: "Email/Password not valid",
+        };
+      } else {
+        return "create an access token";
+      }
+    } else {
+      return {
+        EC: 1,
+        EM: "Email/Password not valid",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 module.exports = {
   createUserService,
+  loginService,
 };
